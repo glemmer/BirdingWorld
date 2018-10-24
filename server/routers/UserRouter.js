d6a48e4eeacd60539d18b1355cb0b97c9c1a1663
glemmer <gert.lemmer@gmail.com>
2018-10-25 03:13:14 +1000

    User Information Added
    Added User model and router
    Added authentication

---
+++ server/routers/UserRouter.js
@@ -0,0 +1,45 @@
+const _ = require('lodash');
+
+var express = require('express');
+var router = express.Router();
+
+var {User} = require('../models/user');
+var {authenticate} = require('../middleware/authenticate');
+
+// middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date.now())
+  next()
+})
+// POST /users
+router.post('/', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+  
+    user.save().then(() => {
+      return user.generateAuthToken();
+    }).then((token) => {
+      res.header('x-auth', token).send(user);
+    }).catch((e) => {
+      res.status(400).send(e);
+    })
+  })
+  
+  router.get('/me', authenticate, (req, res) => {
+    res.send(req.user);
+  })
+  
+  // POST /users/login {email, password}
+  router.post('/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+  
+    User.findByCredentials(body.email, body.password).then((user) => {
+      return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+      });
+    }).catch((e) => {
+      res.status(400).send();
+    });
+  })
+
+  module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var birdRouter = require('./routers/BirdRouter.js');
 var locationRouter = require('./routers/LocationRouter.js');
+var userRouter = require('./routers/UserRouter.js');
 var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 
@@ -15,6 +16,8 @@ app.use('/birds',birdRouter);
 
 //Route for locations
 app.use('/locations',locationRouter);
+
+app.use('/users',userRouter);
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
